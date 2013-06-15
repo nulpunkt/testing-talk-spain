615bb886962a8453f08db0b0253093c3af6145b5
Jesper <nulpunkt@gmail.com>
2013-06-15 12:48:58 +0200

    added logged in feature

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -6,4 +6,14 @@
  */
 class FeatureContext extends Behat\MinkExtension\Context\MinkContext
 {
+	/**
+	 * @Given /^I am logged in as "([^"]*)" with password "([^"]*)"$/
+	 */
+	public function iAmLoggedInAsWithPassword($username, $password)
+	{
+		$this->visit('/auth/');
+		$this->fillField('username', 'jsn@ordbogen.com');
+		$this->fillField('password', 'test123');
+		$this->pressButton('Log in');
+	}
 }
